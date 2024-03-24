a7e9ebbafb0023832daae27d6ead0aeba5a7fd31
farulSP <72846589+farul1@users.noreply.github.com>
2024-03-24 20:35:40 +0700

    Update cloning.blade.php

--- resources/views/cloning.blade.php
+++ resources/views/cloning.blade.php
@@ -1,23 +1,5 @@
 @extends('layouts.main')
 @section('container')
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Prak | {{ $title }}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dosen & Staf » Sistem Informasi - Universitas Telkom Surabaya</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.
-css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css
-">
-  </head>
-  <body>
-
     <div class="bis-sby">
         <div class="div-hero-section">
             <div class="overlap-group">
@@ -193,14 +175,9 @@ css">
                         <div class="p-2"><p class="text-wrapper-8">Build with ♥ by Information System Web Team</p></div>
                       </div>
                     </footer>
+                    </div>
+                </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-
-  </html>
-
 
   @endsection
